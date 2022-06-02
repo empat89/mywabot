1bc0e425b711cadd0af0c9938d06590e3dd70cda
empat89 <f.hsn489@gmail.com>
2022-06-02 13:37:23 +0700

    New

---
+++ config.js
@@ -0,0 +1,37 @@
+/**
+   * Create By Dika Ardnt.
+   * Contact Me on wa.me/6288292024190
+   * Follow https://github.com/DikaArdnt
+   * Recode By Muhammad Fuad Hasan
+   * Contact Me on wa.me/6285161510489
+   * Follow https://Facebook.com/aba.alyaa
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Other
+global.sheet = 'Paste Link AppScript Disini'  //silahkan isi google script app spread sheet nya
+global.blockcall = false //autoblock telepon
+global.welcome = false //pesan selamat datang di group
+global.online = true     //online && mengetik
+global.autoread = true   //ceklis biru
+global.tutup = false //Mode Libur / Di Luar Jam Kerja
+global.owner = ['6285161510489']   //ganti agar fitur bisa di gunakan
+global.sessionName = 'session'  //sesuaikan nama file sesi nya
+global.packname = 'My Sticker'
+global.author = 'WhatsApp Bot'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Berhasil',
+    wait: 'Tunggu Sebentar ...',
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
