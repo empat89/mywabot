bbc92300192f1e909f979196c5209d3e82695d94
empat89 <102323245+empat89@users.noreply.github.com>
2022-07-03 21:32:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,10 +11,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 // Other
-global.projectId = 'mywabot-352516'
-global.heroku = 'Your_Heroku_Apps_Link'
-global.sheet = 'Your_Spread_Sheet_Link'
-global.owner = ['62xxxxxxxxxx']
+global.projectId = 'mywabot-352516'  //default
+global.heroku = 'Your_Heroku_Apps_Link' //if not deployed on heroku, use http://google.com (http ONLY, not https)
+global.sheet = 'Your_Spread_Sheet_Link' //please add your google sheet link with script
+global.owner = ['62xxxxxxxxxx'] //your number, only valid for 1 number
+
+
+
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
 	fs.unwatchFile(file)
